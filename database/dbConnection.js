8cf3ae35dad8cb7feedd964c9c5888a30e9584e7
Pavanii822 <spavanii822@gmail.com>
2025-03-22 13:49:34 +0530

    first commit

---
+++ database/dbConnection.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "./config/config.env" });
+
+export const dbConnection = async() => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("Connected to database!");
+    } catch (err) {
+        console.error(`Some error occurred while connecting to database: ${err}`);
+        process.exit(1);
+    }
+};
